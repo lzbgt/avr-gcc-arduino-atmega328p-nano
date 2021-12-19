be6d43152bff897e9c5eb20d4ec74ab5a0d65bca
Bruce.Lu <lzbgt@icloud.com>
2021-12-19 18:10:54 +0800

    init

--- 2.1-timer_normal/timer.c
+++ 2.1-timer_normal/timer.c
@@ -1,7 +1,22 @@
 /**
  * @file timer.c
  * @author Bruce.Lu (lzbgt@icloud.com)
- * @brief 
+ * @brief timer normal mode to toggle LED
+ * 
+ * As the application note 'atmel-2505' states, timer can be configurated to toggle GPIO pin directly
+ * 
+ * ldi r16, (1<<COM2A1)|(1<<WGM21)|(1<<WGM20)
+ * sts TCCR2A,r16 ; OC2A toggling on compare match/timer
+ * ; Clock = system clock
+ * ldi r16,32
+ * sts OCR2A,r16 ; Set output compare value to 32
+ * 
+ * TOV = Fck/(Prescaler * (2^(res-1))) = Fck/p/255/2 => 30Hz, assuming the max prescaler is used (1024) and 16MHz system clock,
+ * the result longest piroid is about 33ms it is too short for LED blinking.
+ * 
+ * So we need to Accumuate TOV in ISR to devide TOV frequency futher.
+ * 
+ * 
  * @version 0.1
  * @date 2021-12-19
  * 
@@ -22,44 +37,52 @@ will result in a toggle in the corresponding bit in the Data Register
 */
 #define LED_TOGGLE PINB |= _BV(PINB5)
 
+uint8_t freq = F_CPU / 1024 / 256 / 2;  // 30
+
+// NOTE: the best resolution is 30Hz, about 33ms, so ms can't be less than 33
+uint16_t calc_scalar(uint16_t ms) {
+    if (ms < 33) {
+        ms = 33;
+    }
+    return ms * freq / 1000;
+}
+
+volatile uint16_t scaler_ref = 33;
+volatile uint16_t scaler = 33;  //< holds up max period of 2184 seconds
+                                //< uint8_t can only hold up for 8 seconds.
 // Interrupt Service Routing
-// we use PCINT1(PB1) as Pin Change Interrupt, it belongs to vect0 (PINTC[0:7])
-ISR(PCINT0_vect) {
-    // remember input pin with pull-up is normal HI,
-    // so we dectect PIN changed to LOW
-    if (!(PINB & _BV(PB1))) {
-        // PB1 reads LOW, LED on
-        LED_ON;
-    } else {
-        // reads HI
-        LED_OFF;
+ISR(TIMER0_OVF_vect) {
+    if (--scaler == 0) {
+        scaler = scaler_ref;
+        LED_TOGGLE;
     }
 }
 
 int main(void) {
-    // explicity set PB1 input pull-up mode, to enable PB1 normal HI
-    // avoid input tri-state(HI-z) effect
-    DDRB &= ~_BV(DDB1);
-    PORTB |= _BV(PB1);
-
-    // PCINT1(PB1) belongs to PCINT[0:7] and thus MASK bank 0 and ICR bank 0
-    PCMSK0 |= _BV(PCINT1);
-    PCICR |= _BV(PCIE0);
+    scaler_ref = calc_scalar(1000);
+    scaler = scaler_ref;
+    /* Timer clock = I/O clock / 1024 */
+    TCCR0B = (1 << CS02) | (1 << CS00);
+    /* Clear overflow flag */
+    TIFR0 = 1 << TOV0;
+    /* Enable Overflow Interrupt */
+    TIMSK0 = 1 << TOIE0;
 
     // set PB5(LED) output mode
     DDRB |= _BV(DDB5);
+
+    // enable power saving
+    PRR = 0xFF & (~(_BV(PRTIM0) | (_BV(PRTWI))));
+    set_sleep_mode(SLEEP_MODE_IDLE);
+    sleep_enable();
+    sleep_bod_disable();
 
     // enable ISR
     sei();
 
     while (1) {
-        // enable power saving
-        PRR = 0xFF;
-        set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-        sleep_enable();
-        sleep_bod_disable();
-        sei();
         sleep_cpu();
         sleep_disable();
+        sei();
     }
 }
